69219a061ff4c627263f6b0e1500d7df3a01cd15
Komputer\Володька <anohin-vladimir@Mail.ru>
2017-03-18 18:38:35 +0300

    Revert "Revert "Commit 1""
    This reverts commit ffb985343afb31e62fa9547e609198fe0facbbe1.

---
+++ Assets/Scripts/Count2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Count2 : MonoBehaviour {
+
+	private int score;
+
+	void Update(){
+		if (Cell.player2 == true){
+			score++;
+			GetComponent<Text> ().text = score.ToString();
+			Cell.player2 = false;
+		} else if (CellAi.player2 == true){
+			score++;
+			GetComponent<Text> ().text = score.ToString();
+			CellAi.player2 = false;
+
+		}  else if (CellAiToe.player1 == true){
+			score++;
+			GetComponent<Text> ().text = score.ToString();
+			CellAiToe.player1 = false;
+
+		}
+	}
+
+}
